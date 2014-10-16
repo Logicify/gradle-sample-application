508b2a3865422c99c83370f1486e99c284c6779e
Dmitry Berezovsky <dmitri.berezovsky@logicify.com>
2014-10-16 16:29:40 +0300

    Added simple domain class and adjusted build scenario

---
+++ gtp/gtp-domain/grails-app/domain/com/logicify/demo/gtp/domain/Book.groovy
@@ -0,0 +1,11 @@
+package com.logicify.demo.gtp.domain
+
+/**
+ * Created by LOGICIFY\corvis on 10/16/14.
+ */
+class Book {
+    String title;
+    String author;
+    String tags;
+    String description;
+}
--- gtp/gtp-web/grails-app/conf/BootStrap.groovy
+++ gtp/gtp-web/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import com.logicify.demo.gtp.domain.Book
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (!Book.count()) {
+            new Book(title: 'Book 1', author: 'Author 1', description: 'some description')
+        }
     }
     def destroy = {
     }
