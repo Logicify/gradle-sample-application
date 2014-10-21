f7b61b587ed40c5745efedf878e1b368a31bf6d1
Dmitry Berezovsky <dmitri.berezovsky@logicify.com>
2014-10-21 22:32:53 +0300

    Added Home controller

---
+++ gtp/gtp-web/grails-app/controllers/com/logicify/demo/gtp/controller/HomePageController.groovy
@@ -0,0 +1,10 @@
+package com.logicify.demo.gtp.controller
+
+/**
+ * Created by LOGICIFY\corvis on 10/16/14.
+ */
+class HomePageController {
+    def index() {
+        render view: '/home'
+    }
+}
